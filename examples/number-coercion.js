e27b7500165690e897f449d23598b291fb6945f2
Gabriel Llamas <gabriel_llamas_llopis@yahoo.es>
2013-07-22 14:32:00 +0200

    updated number-coercion

--- examples/number-coercion.js
+++ examples/number-coercion.js
@@ -4,10 +4,14 @@ var speedy = require ("../lib");
 
 speedy.run ({
 	Number: function (){
-		Number ("0x10");
+		Number ("16");
 	},
 	"+": function (){
-		+"0x10";
+		+"16";
+	},
+	parseInt: function (){
+		//It's not the same as Number but shares some functionalities
+		parseInt ("16");
 	}
 });
 
@@ -18,18 +22,20 @@ Node v0.10.13
 V8 v3.14.5.9
 Speedy v0.0.7
 
-Benchmarks: 2
+Benchmarks: 3
 Timeout: 1000ms (1s 0ms)
 Samples: 3
 Total time per benchmark: ~3000ms (3s 0ms)
-Total time: ~6000ms (6s 0ms)
+Total time: ~9000ms (9s 0ms)
 
 Higher is better (ops/sec)
 
 Number
-  17,611,244 ± 0.1%
+  39,851,676 ± 0.0%
 +
-  16,173,311 ± 0.0%
+  32,630,632 ± 0.0%
+parseInt
+  58,444,256 ± 0.0%
 
-Elapsed time: 6051ms (6s 51ms)
+Elapsed time: 9078ms (9s 78ms)
 */
