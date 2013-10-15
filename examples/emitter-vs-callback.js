25bc075ce60f63fa02cfb7ef2336e6d61857ea11
Gabriel Llamas <gabriel_llamas_llopis@yahoo.es>
2013-10-15 15:58:15 +0200

    updated emitter-vs-callback

--- examples/emitter-vs-callback.js
+++ examples/emitter-vs-callback.js
@@ -4,20 +4,32 @@ var events = require ("events");
 var util = require ("util");
 var speedy = require ("../lib");
 
-var Emitter = function (){
+var EmitterOn = function (){
 	events.EventEmitter.call (this);
 };
 
-util.inherits (Emitter, events.EventEmitter);
+util.inherits (EmitterOn, events.EventEmitter);
 
-Emitter.prototype.ping = function (){
+EmitterOn.prototype.ping = function (){
 	this.emit ("ping");
 };
 
-var emitter = new Emitter ();
-emitter.on ("ping", function (){
-	 this._done ();
-});
+var emitterOn = new EmitterOn ()
+		.on ("ping", function (){
+			this._done ();
+		});
+
+var EmitterOnce = function (){
+	events.EventEmitter.call (this);
+};
+
+util.inherits (EmitterOnce, events.EventEmitter);
+
+EmitterOnce.prototype.ping = function (){
+	this.emit ("ping");
+};
+
+var emitterOnce = new EmitterOnce ();
 
 var Callback = function (){};
 
@@ -28,9 +40,13 @@ Callback.prototype.ping = function (){
 var callback = new Callback ();
 
 speedy.run ({
-	emitter: function (done){
-		emitter._done = done;
-		emitter.ping ();
+	"emitter-on": function (done){
+		emitterOn._done = done;
+		emitterOn.ping ();
+	},
+	"emitter-once": function (done){
+		emitterOnce.once ("ping", done);
+		emitterOnce.ping ();
 	},
 	callback: function (done){
 		callback._done = done;
@@ -39,24 +55,34 @@ speedy.run ({
 });
 
 /*
+When an event is emitted it loops through all the handlers, that's why is a
+little slower.
+
+on() is also a way more inconsistent compared with callbacks, don't ask why.
+once() performs a "delete", that's why it is a lot more slower.
+*/
+
+/*
 File: emitter-vs-callback.js
 
 Node v0.10.20
 V8 v3.14.5.9
 Speedy v0.1.0
 
-Tests: 2
+Tests: 3
 Timeout: 1000ms (1s 0ms)
 Samples: 3
 Total time per test: ~3000ms (3s 0ms)
-Total time: ~6000ms (6s 0ms)
+Total time: ~9000ms (9s 0ms)
 
 Higher is better (ops/sec)
 
-emitter
-  684,655 ± 0.4%
+emitter-on
+  4,282,478 ± 10.4%
+emitter-once
+  672,740 ± 0.2%
 callback
-  5,535,242 ± 0.0%
+  5,762,500 ± 0.1%
 
-Elapsed time: 6053ms (6s 53ms)
+Elapsed time: 9078ms (9s 78ms)
 */
