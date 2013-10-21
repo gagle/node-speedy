a3fbebae201ca702055fa7e318999818feb3acef
Gabriel Llamas <gabriel_llamas_llopis@yahoo.es>
2013-10-21 22:26:43 +0200

    updated in-object

--- examples/in-object.js
+++ examples/in-object.js
@@ -10,28 +10,33 @@ speedy.run ({
 	},
 	"undefined": function (){
 		o.a !== undefined;
+	},
+	"hasOwnProperty": function (){
+		o.hasOwnProperty ("a");
 	}
 });
 
 /*
 File: in-object.js
 
-Node v0.10.20
+Node v0.10.21
 V8 v3.14.5.9
-Speedy v0.1.0
+Speedy v0.1.1
 
-Tests: 2
+Tests: 3
 Timeout: 1000ms (1s 0ms)
 Samples: 3
 Total time per test: ~3000ms (3s 0ms)
-Total time: ~6000ms (6s 0ms)
+Total time: ~9000ms (9s 0ms)
 
 Higher is better (ops/sec)
 
 in
-  16,979,118 ± 0.0%
+  16,356,927 ± 0.5%
 undefined
-  151,003,184 ± 0.0%
+  151,198,856 ± 0.0%
+hasOwnProperty
+  26,712,654 ± 0.0%
 
-Elapsed time: 6146ms (6s 146ms)
+Elapsed time: 9079ms (9s 79ms)
 */
