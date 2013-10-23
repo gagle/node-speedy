6d88576e15f7d5eadadb26c9c2df0590ac78e551
Gabriel Llamas <gabriel_llamas_llopis@yahoo.es>
2013-10-23 12:30:52 +0200

    updated parse-number

--- examples/parse-number.js
+++ examples/parse-number.js
@@ -9,6 +9,12 @@ speedy.run ({
 	"+": function (){
 		+"1.1";
 	},
+	"~~": function (){
+		//It's the same as "+" but it also crops the decimal part, so if you need
+		//to sanitize a number, instead of doing "parseInt + decimal removal", it's
+		//faster using the tildes "~~"
+		~~"1.1";
+	},
 	parseFloat: function (){
 		//It's not the same as Number but shares some similarities
 		parseFloat ("1.1");
@@ -21,26 +27,28 @@ speedy.run ({
 /*
 File: parse-number.js
 
-Node v0.10.20
+Node v0.10.21
 V8 v3.14.5.9
-Speedy v0.1.0
+Speedy v0.1.1
 
-Tests: 4
+Tests: 5
 Timeout: 1000ms (1s 0ms)
 Samples: 3
 Total time per test: ~3000ms (3s 0ms)
-Total time: ~12000ms (12s 0ms)
+Total time: ~15000ms (15s 0ms)
 
 Higher is better (ops/sec)
 
 Number
-  13,078,063 ± 0.1%
+  13,014,401 ± 0.1%
 +
-  10,982,975 ± 0.0%
+  11,078,243 ± 0.0%
+~~
+  11,337,886 ± 0.0%
 parseFloat
-  16,901,142 ± 0.0%
+  16,981,702 ± 0.0%
 json
-  10,823,115 ± 0.0%
+  10,712,992 ± 0.2%
 
-Elapsed time: 12293ms (12s 293ms)
+Elapsed time: 15366ms (15s 366ms)
 */
